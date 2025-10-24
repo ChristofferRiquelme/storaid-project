93f7b1acbf9a8e144ced2d0b0e1e13dd143980cd
ChristofferRiquelme <christoffer.riquelme@gmail.com>
2025-10-24 17:56:54 +0200

    Started on header, repsonsive next

---
+++ src/components/Header.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import phoneIcon from '../assets/phone-icon.svg'
+import mailIcon from '../assets/mail-icon.svg'
+import facebookIcon from '../assets/facebook-icon.svg'
+import xIcon from '../assets/x-icon.svg'
+import instagramIcon from '../assets/instagram-icon.svg'
+import youtubeIcon from '../assets/youtube-icon.svg'
+
+function Header() {
+  return (
+    <div className="header">
+        <div className="header-item">
+            <div className="header-item-phone">
+                <div className="header-item-phone-image">
+                    <img src={phoneIcon} alt="Phone Icon" />
+                </div>
+                <div className="header-item-phone-number">
+                    <a href="callto:+46812312244">+46 8 123 122 44</a>
+                </div>
+            </div>
+            <div className="header-item-mail">
+                <div className="header-item-mail-image">
+                    <img src={mailIcon} alt="Mail Icon" />
+                </div>
+                <div className="header-item-mail-address">
+                    <a href="mailto:contact@domain.com">contact@domain.com</a>
+                </div>
+            </div>
+        </div>
+
+        <div className="header-item">
+            <div className="header-item-social">
+                <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer"><img src={facebookIcon} alt="Facebook Icon" /></a>
+            </div>
+            <div className="header-item-social">
+                <a href="https://www.x.com" target="_blank" rel="noopener noreferrer"><img src={xIcon} alt="X Icon" /></a>
+            </div>
+            <div className="header-item-social">
+                <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer"><img src={instagramIcon} alt="Instagram Icon" /></a>
+            </div>
+            <div className="header-item-social">
+                <a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer"><img src={youtubeIcon} alt="YouTube Icon" /></a>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-import Footer from './components/Footer.jsx'
+import Header from './components/Header.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Footer />
+    <Header />
   </StrictMode>,
 )
