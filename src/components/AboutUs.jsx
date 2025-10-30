cab8f7b095bdf9669f02627ee1aa50ce9f1ba229
ChristofferRiquelme <christoffer.riquelme@gmail.com>
2025-10-30 16:28:41 +0100

    Added html

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function AboutUs() {
+  return (
+    <section className='about-us'>
+        <div className="about-us-inner">
+            <div className="about-us-items">
+                <div className="about-us-item">
+                    <div className="about-us-item-image1"></div>
+                    <div className="about-us-item-image2"></div>
+                </div>
+                <div className="about-us-item">
+                    <div className="about-us-item-text"></div>
+                    <div className="about-us-item-cards"></div>
+                    <div className="about-us-item-info"></div>
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default AboutUs
