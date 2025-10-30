739828bdbd25a02eb154da285894e8a6d0389051
ChristofferRiquelme <christoffer.riquelme@gmail.com>
2025-10-30 16:56:07 +0100

    Updated html

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import AboutUsImage from '../assets/about-us-image.svg'
+import SecondaryButton from './SecondaryButton'
+import YellowPhoneIcon from '../assets/yellow-phone-icon.svg'
 
 function AboutUs() {
   return (
@@ -6,13 +9,51 @@ function AboutUs() {
         <div className="about-us-inner">
             <div className="about-us-items">
                 <div className="about-us-item">
-                    <div className="about-us-item-image1"></div>
-                    <div className="about-us-item-image2"></div>
+                    <div className="about-us-item-image">
+                        <img src={AboutUsImage} alt="About Us Image" />
+                    </div>
+                    <div className="about-us-item-stats">
+                        <div className="about-us-item-stats-experience">
+                            <h2>12+</h2>
+                            <p>Years of Experience</p>
+                        </div>
+                        <div className="about-us-item-stats-clients">
+                            <h2>150K+</h2>
+                            <p>Satisfied Clients</p>
+                        </div>
+                        <div className="about-us-item-stats-warehouses">
+                            <h2>35+</h2>
+                            <p>Warehouses</p>
+                        </div>
+                    </div>
                 </div>
+
                 <div className="about-us-item">
-                    <div className="about-us-item-text"></div>
-                    <div className="about-us-item-cards"></div>
-                    <div className="about-us-item-info"></div>
+                    <div className="about-us-item-text">
+                        <h4>About Us</h4>
+                        <h3>Providing a Safe Space for Your Treasured Items</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie nisl sed dui lacinia gravida. Nulla quis nulla leo. Mauris ac blandit nisi, non sodales augue. Phasellus eget elit gravida.</p>
+                    </div>
+                    <div className="about-us-item-cards">
+                        <div className="about-us-item-card">
+                            <h5>Vision</h5>
+                            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusan tium doloremque laudantium.</p>
+                        </div>
+                        <div className="about-us-item-card">
+                            <h5>Mission</h5>
+                            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusan tium doloremque laudantium.</p>
+                        </div>
+                    </div>
+                    <div className="about-us-item-info">
+                        <SecondaryButton text="Discover More" />
+                        <div className="about-us-item-info-contact">
+                            <img src={YellowPhoneIcon} alt="Yellow Phone Icon" />
+                            <div className="about-us-item-info-contact-phone">
+                                <h5>More Information</h5>
+                                <h6>+46 8 123 122 44</h6>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import Header from './components/Header.jsx'
 import Nav from './components/Nav.jsx'
 import Hero from './components/Hero.jsx'
 import heroImage from './assets/hero-image.svg'
+import AboutUs from './components/AboutUs.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -16,5 +17,6 @@ createRoot(document.getElementById('root')).render(
       text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a sem magna. Etiam ac odio sit amet lorem fringilla sodales. Cras lobortis, libero at iaculis luctus, nisi ex pellentesque nisi, at euismod sem ipsum ac dolor."
       image={heroImage}
     />
+    <AboutUs />
   </StrictMode>,
 )
