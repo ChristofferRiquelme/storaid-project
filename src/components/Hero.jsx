8e6405cd83f07ad920c934fa710d1222e917750e
ChristofferRiquelme <christoffer.riquelme@gmail.com>
2025-10-30 14:21:47 +0100

    Hero component done

---
+++ src/components/Hero.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Hero({ title, subtitle, text, image }) {
+  return (
+    <section className='hero'>
+        <div className="hero-inner">
+            <div className="hero-content">
+                {subtitle && <h4 className="hero-subtitle">{subtitle}</h4>}
+                {title && <h1 className="hero-title">{title}</h1>}
+                {text && <p className="hero-text">{text}</p>}
+            </div>
+
+        {image && (
+            <div className="hero-image">
+                <img src={image} alt="Hero Image" />
+            </div>
+        )}
+        </div>
+    </section>
+  )
+}
+
+export default Hero
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Header from './components/Header.jsx'
 import Nav from './components/Nav.jsx'
+import Hero from './components/Hero.jsx'
+import heroImage from './assets/hero-image.svg'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header />
     <Nav />
+    <Hero
+      title="Space Simplified, Storage Perfected"
+      subtitle="Welcome to StorAid"
+      text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a sem magna. Etiam ac odio sit amet lorem fringilla sodales. Cras lobortis, libero at iaculis luctus, nisi ex pellentesque nisi, at euismod sem ipsum ac dolor."
+      image={heroImage}
+    />
   </StrictMode>,
 )
