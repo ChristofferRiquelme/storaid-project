b2d9e31685eaaa45f17e4ae478b50d314d9287e1
ChristofferRiquelme <christoffer.riquelme@gmail.com>
2025-11-03 17:07:32 +0100

    Updated brands component

--- src/components/Brands.jsx
+++ src/components/Brands.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
-import BrandsImage from '../assets/brands.svg'
+import BrandImage1 from '../assets/brand1.svg'
+import BrandImage2 from '../assets/brand2.svg'
+import BrandImage3 from '../assets/brand3.svg'
+import BrandImage4 from '../assets/brand4.svg'
+import BrandImage5 from '../assets/brand5.svg'
 
 function Brands() {
   return (
     <section className='brands'>
         <div className='brands-inner'>
-            <img src={BrandsImage} alt="" />
+            <div className="brands-item">
+                <img src={BrandImage1} alt="" />
+            </div>
+            <div className="brands-item">
+                <img src={BrandImage2} alt="" />
+            </div>
+            <div className="brands-item">
+                <img src={BrandImage3} alt="" />
+            </div>
+            <div className="brands-item">
+                <img src={BrandImage4} alt="" />
+            </div>
+            <div className="brands-item">
+                <img src={BrandImage5} alt="" />
+            </div>
         </div>
     </section>
   )
