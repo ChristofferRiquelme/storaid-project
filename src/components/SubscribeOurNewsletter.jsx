365ae1c34387229ddebe5071e4f7d8bd470fabf5
ChristofferRiquelme <christoffer.riquelme@gmail.com>
2025-11-11 20:59:06 +0100

    Added css and responsive

--- src/components/SubscribeOurNewsletter.jsx
+++ src/components/SubscribeOurNewsletter.jsx
@@ -9,8 +9,11 @@ function SubscribeOurNewsletter() {
                 <p>Subscribe to our newsletter to receive early discount offers, updates and info</p>
             </div>
             <div className="subscribe-our-newsletter-input-and-button">
-                <input type="email" placeholder="Enter your email" className='subscribe-our-newsletter-input' />
-                <button type="submit" className='subscribe-our-newsletter-button'>Submit</button>
+                    <div className="input-wrapper">
+                        <input id='subscribe-input' className='subscribe-our-newsletter-input' type="email" placeholder="Enter your email" />
+                        <span className="asterisk">*</span>
+                    </div>
+                <button className='subscribe-our-newsletter-button' type="submit">Submit</button>
             </div>
         </div>
     </section>
