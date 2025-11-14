5ad6e92ada63668ee193556e54aae4e629ff1da0
ChristofferRiquelme <christoffer.riquelme@gmail.com>
2025-11-14 15:04:34 +0100

    Added get in touch form component

---
+++ src/components/GetInTouchForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function GetInTouchForm() {
+  return (
+    <div>GetInTouchForm</div>
+  )
+}
+
+export default GetInTouchForm
--- src/main.jsx
+++ src/main.jsx
@@ -12,14 +12,11 @@ import Testimonials from './components/Testimonials.jsx'
 import TestimonialsHeroImage from './assets/testimonials-hero-image.svg'
 import WhyChooseUs from './components/WhyChooseUs.jsx'
 import PricingPlan from './components/PricingPlan.jsx'
-import PricingCard from './components/PricingCard.jsx'
-import ThirdButton from './components/ThirdButton.jsx'
 import BookNow from './components/BookNow.jsx'
 import BlogAndNews from './components/BlogAndNews.jsx'
 import SubscribeOurNewsletter from './components/SubscribeOurNewsletter.jsx'
 import Footer from './components/Footer.jsx'
-import Faq from './components/Faq.jsx'
-import FindUs from './components/FindUs.jsx'
+import GetInTouchForm from './components/GetInTouchForm.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -45,6 +42,6 @@ createRoot(document.getElementById('root')).render(
     <BlogAndNews />
     <SubscribeOurNewsletter />
     <Footer />
-    <FindUs />
+    <GetInTouchForm />
   </StrictMode>,
 )
